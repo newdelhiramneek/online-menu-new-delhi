7c0f1228526cfb81a5f4c298ac8c2098040360a1
namanjeet <namanshuttler@gmail.com>
2025-12-12 19:40:54 +0530

    modified footer

--- src/data/siteCopy.js
+++ src/data/siteCopy.js
@@ -8,15 +8,15 @@
       outro:
         "Jedes Gericht erzählt eine Geschichte von Gewürzen, Farbe und Harmonie."
     },
-    addToCart: "Add To List",
+    addToCart: "Zur Liste hinzufügen",
     emptyState: "In dieser Kategorie sind aktuell keine Gerichte hinterlegt.",
     footer: {
       addressHeading: "New Delhi",
-      addressLines: ["Beispielstraße 12", "12345 Musterstadt"],
+      addressLines: ["Pfotenhauer Straße 70", "01069 Dresden, Germany"],
       hoursHeading: "Öffnungszeiten",
       hoursLines: ["Mo-Fr: 11:30-22:30", "Sa-So: 12:00-23:00"],
       contactHeading: "Kontakt",
-      contactLines: ["Tel: 01234 / 567890", "E-Mail: info@newdelhi.de"],
+      contactLines: ["Tel: +49 1521 4704642", "E-Mail: hallo.newdelhi@gmail.com"],
       allergenHeading: "Allergenlegende",
       allergenLegend:
         "A - glutenhaltiges Getreide • B - Krebstiere • C - Eier • D - Fisch • E - Erdnüsse • F - Soja • G - Milch • H - Schalenfrüchte • I - Sellerie • J - Senf • K - Sesamsamen • L - Schwefeldioxid & Sulfite • M - Lupinen • N - Weichtiere",
@@ -38,11 +38,11 @@
     emptyState: "No dishes in this category yet.",
     footer: {
       addressHeading: "New Delhi",
-      addressLines: ["Example Street 12", "12345 Sample City"],
+      addressLines: ["Pfotenhauer Straße 70", "01069 Dresden, Germany"],
       hoursHeading: "Opening hours",
       hoursLines: ["Mon-Fri: 11:30-22:30", "Sat-Sun: 12:00-23:00"],
       contactHeading: "Contact",
-      contactLines: ["Tel: 01234 / 567890", "Email: info@newdelhi.de"],
+      contactLines: ["Tel: +49 1521 4704642", "Email: hallo.newdelhi@gmail.com"],
       allergenHeading: "Allergen legend",
       allergenLegend:
         "A - cereals containing gluten • B - crustaceans • C - eggs • D - fish • E - peanuts • F - soybeans • G - milk • H - nuts • I - celery • J - mustard • K - sesame seeds • L - sulphur dioxide & sulphites • M - lupin • N - molluscs",
