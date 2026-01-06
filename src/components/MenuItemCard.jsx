9bcff991f417a69c10f7ba8b419fb9ff34c08116
namanjeet <namanshuttler@gmail.com>
2026-01-07 01:17:45 +0530

    commit

--- src/components/MenuItemCard.jsx
+++ src/components/MenuItemCard.jsx
@@ -15,49 +15,57 @@ const MenuItemCard = ({ item, addToCartLabel, onAddToList }) => {
   return (
     <article className={cardClass}>
       <header className="menu-item-header">
-        <h3 className="menu-item-name">{item.name}</h3>
-        {hasSizes ? (
-          <div className="menu-item-prices">
-            {item.sizes.map(({ label, price }) => (
-              <div key={`${item.id}-${label}`} className="menu-item-price-row">
-                <span className="menu-item-size">{label}</span>
-                <span className="menu-item-price">{price}</span>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <span className="menu-item-price">{item.price}</span>
-        )}
-      </header>
-
-      <p className="menu-item-description">{item.description}</p>
-
-      {(item.dietary && item.dietary.length > 0) || !isVeg ? (
-        <div className="menu-item-tags">
-          {item.dietary &&
-            item.dietary.map((tag) => (
-              <span
-                key={tag}
-                className={
-                  "menu-item-tag" + (isVeg ? " menu-item-tag--veg" : " menu-item-tag--nonveg")
-                }
-              >
-                {tag}
-              </span>
-            ))}
-          {!isVeg && (
-            <span className="menu-item-tag menu-item-tag--nonveg">Non Veg</span>
+        <div className="menu-item-title">
+          <h3 className="menu-item-name">{item.name}</h3>
+        </div>
+        <div className="menu-item-price-block">
+          {hasSizes ? (
+            <div className="menu-item-prices">
+              {item.sizes.map(({ label, price }) => (
+                <div key={`${item.id}-${label}`} className="menu-item-price-row">
+                  <span className="menu-item-size">{label}</span>
+                  <span className="menu-item-price">{price}</span>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <span className="menu-item-price">{item.price}</span>
           )}
         </div>
-      ) : null}
+      </header>
 
-      <button
-        className="btn-ghost"
-        type="button"
-        onClick={() => onAddToList && onAddToList(item)}
-      >
-        {ctaLabel}
-      </button>
+      <div className="menu-item-body">
+        <p className="menu-item-description">{item.description}</p>
+
+        {(item.dietary && item.dietary.length > 0) || !isVeg ? (
+          <div className="menu-item-tags">
+            {item.dietary &&
+              item.dietary.map((tag) => (
+                <span
+                  key={tag}
+                  className={
+                    "menu-item-tag" + (isVeg ? " menu-item-tag--veg" : " menu-item-tag--nonveg")
+                  }
+                >
+                  {tag}
+                </span>
+              ))}
+            {!isVeg && (
+              <span className="menu-item-tag menu-item-tag--nonveg">Non Veg</span>
+            )}
+          </div>
+        ) : null}
+      </div>
+
+      <div className="menu-item-actions">
+        <button
+          className="btn-ghost"
+          type="button"
+          onClick={() => onAddToList && onAddToList(item)}
+        >
+          {ctaLabel}
+        </button>
+      </div>
     </article>
   );
 };
